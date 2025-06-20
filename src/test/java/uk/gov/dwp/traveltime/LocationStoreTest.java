07428c8074b5891c4441a023baaea3b7b1b95ea4
Julian Brown <julian@the-lair.info>
2025-06-20 16:33:30 +0100

    chore: prevent inspection warning on mock reset

--- src/test/java/uk/gov/dwp/traveltime/LocationStoreTest.java
+++ src/test/java/uk/gov/dwp/traveltime/LocationStoreTest.java
@@ -1,14 +1,29 @@
 package uk.gov.dwp.traveltime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class LocationStoreTest {
     LocationStore locations;
+
+    @Mock
+    Map<String, Map<String, RouteStore>> mockSites;
+
+    @BeforeEach
+    public void setup() {
+        //noinspection unchecked
+        reset(mockSites);
+    }
 
     @Test
     public void addLocationAddsEntryToLocationList() {
@@ -61,5 +76,34 @@ public class LocationStoreTest {
         Map<String, RouteStore> destinations = sites.get("London");
         RouteStore destination = destinations.get("Paris");
         assertEquals("03:20",destination.getAverage());
+    }
+
+    @Test
+    public void addRouteAmendsExistingToLocations() {
+        String fromSite = "Manchester";
+        String toSite = "London";
+        HashMap<String, RouteStore> dummy = new HashMap<>();
+        dummy.put(toSite, new RouteStore(toSite, new SimpleAverageRoute()));
+        when(mockSites.containsKey(fromSite)).thenReturn(true);
+        when(mockSites.get(fromSite)).thenReturn(dummy);
+        locations = new LocationStore(mockSites);
+        locations.addRoute(fromSite, toSite, "03:20");
+        verify(mockSites, times(0)).put(fromSite, null);
+        verify(mockSites, times(1)).put(toSite, null);
+    }
+
+    @Test
+    public void addRouteAmendsExistingFromLocations() {
+        String fromSite = "Manchester";
+        String toSite = "London";
+        HashMap<String, RouteStore> dummy = new HashMap<>();
+        dummy.put(toSite, new RouteStore(toSite, new SimpleAverageRoute()));
+        when(mockSites.containsKey(toSite)).thenReturn(true);
+        when(mockSites.containsKey(fromSite)).thenReturn(false);
+        when(mockSites.get(fromSite)).thenReturn(dummy);
+        locations = new LocationStore(mockSites);
+        locations.addRoute(fromSite, toSite, "03:20");
+        verify(mockSites, times(1)).put(anyString(), any());
+        verify(mockSites, times(0)).put(toSite, null);
     }
 }
